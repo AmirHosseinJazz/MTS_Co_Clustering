c41fdbae5fa4b9209e26fb92931315f59fbad8eb
Jaz <amirhossein.jazayeri@gmail.com>
2024-05-14 11:21:55 +0200

    data

--- TVAE/main.py
+++ TVAE/main.py
@@ -17,4 +17,14 @@ def main(params):
     else :
         raise ValueError('Unsupported data source. Use 12var or 29var')
     
+    sequence_length = data.shape[1]
+    print('Parameter Sequence Length:',sequence_length)
+    number_of_features = data.shape[2]
+    print('Parameter Number of Features:',number_of_features)
+    Y_temporal=[len(x) for x in data]
+    dataset=TensorDataset(torch.tensor(data).float())
+    params['input_dim'] = number_of_features
+    params['max_seq_len'] = sequence_length
+    params['num_samples']=data.shape[0]
+    dataloader=torch.utils.data.DataLoader(dataset,batch_size=params['batch_size'],shuffle=True)
     
