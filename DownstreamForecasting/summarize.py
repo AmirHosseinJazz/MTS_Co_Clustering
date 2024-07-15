b8e9b50e9c0106a205c98efb0321312b9ece11f5
Jaz <amirhossein.jazayeri@gmail.com>
2024-07-15 10:44:08 +0200

    Data removed from repo

--- DownstreamForecasting/summarize.py
+++ DownstreamForecasting/summarize.py
@@ -24,6 +24,40 @@ def summarize_results():
                         "MSE": temp["MSE"].mean(),
                     }
                 )
+            elif "raw_data" in k:
+                temp = pd.read_csv(f"./evaluation_results/{k}")
+                # take date out of k regex
+                pattern = r"(\d{4}-\d{2}-\d{2})_(\d{2}-\d{2}-\d{2})"
+                match = re.search(pattern, k)
+                if match:
+                    date = match.group(0)
+                else:
+                    date = "None"
+                results.append(
+                    {
+                        "Technique": "Clustering",
+                        "Type": "Complex_Network_Raw_Data",
+                        "Date": date,
+                        "MSE": temp["MSE"].mean(),
+                    }
+                )
+            elif "embedded" in k:
+                temp = pd.read_csv(f"./evaluation_results/{k}")
+                # take date out of k regex
+                pattern = r"(\d{4}-\d{2}-\d{2})_(\d{2}-\d{2}-\d{2})"
+                match = re.search(pattern, k)
+                if match:
+                    date = match.group(0)
+                else:
+                    date = "None"
+                results.append(
+                    {
+                        "Technique": "Clustering",
+                        "Type": "Complex_Network_Embedded",
+                        "Date": date,
+                        "MSE": temp["MSE"].mean(),
+                    }
+                )
         if k.split("_")[0] == "Co-Clustering":
             if "Embedded" in k:
                 temp = pd.read_csv(f"./evaluation_results/{k}")
